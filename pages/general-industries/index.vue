cd27450d2a547c493ce94acd57b98638b5ff739f
CeeDyyy <thanawat.ud@mail.kmutt.ac.th>
2023-02-15 19:21:35 +0700

    use col for margin in genera-industries

--- pages/general-industries/index.vue
+++ pages/general-industries/index.vue
@@ -6,54 +6,72 @@
     <v-main>
       <v-container>
         <v-row>
-          <v-col v-for="item in industries.industries" :key="item.id" cols="12" sm="4">
-            <v-hover v-slot="{ isHovering, props }">
-              <v-card
-                class="mx-auto"
-                color="grey-lighten-4"
-                max-width="600"
-                v-bind="props"
+          <v-col cols="12" lg="2"> </v-col>
+
+          <v-col cols="12" lg="8">
+            <v-row>
+              <v-col
+                v-for="item in industries.industries"
+                :key="item.id"
+                cols="12"
+                sm="4"
               >
-                <v-img :aspect-ratio="3 / 2" cover :src="item.img">
-                  <v-expand-transition>
-                    <div
-                      v-if="isHovering"
-                      class="
-                        d-flex
-                        transition-fast-in-fast-out
-                        bg-yellow-darken-1
-                        text-green-darken-4
-                        v-card--reveal
-                        text-h4
-                      "
-                      style="height: 100%"
-                    >
-                      {{ industries.learnmore }}
-                    </div>
-                  </v-expand-transition>
-                </v-img>
-
-                <v-card-text class="pt-6">
-                  <div
-                    class="text-h6 font-weight-light text-green-darken-4 mb-2"
+                <v-hover v-slot="{ isHovering, props }">
+                  <v-card
+                    class="mx-auto"
+                    color="grey-lighten-4"
+                    max-width="600"
+                    v-bind="props"
                   >
-                    {{ item.title }}
-                  </div>
+                    <v-img :aspect-ratio="3 / 2" cover :src="item.img">
+                      <v-expand-transition>
+                        <div
+                          v-if="isHovering"
+                          class="
+                            d-flex
+                            transition-fast-in-fast-out
+                            bg-yellow-darken-1
+                            text-green-darken-4
+                            v-card--reveal
+                            text-h4
+                          "
+                          style="height: 100%"
+                        >
+                          {{ industries.learnmore }}
+                        </div>
+                      </v-expand-transition>
+                    </v-img>
 
-                  <div
-                    class="
-                      text-subtitle-2
-                      font-weight-light
-                      text-grey-darken-2
-                      mb-2
-                    "
-                  >
-                    {{ item.content }}
-                  </div>
-                </v-card-text>
-              </v-card>
-            </v-hover>
+                    <v-card-text class="pt-6">
+                      <div
+                        class="
+                          text-h6
+                          font-weight-light
+                          text-green-darken-4
+                          mb-2
+                        "
+                      >
+                        {{ item.title }}
+                      </div>
+
+                      <div
+                        class="
+                          text-subtitle-2
+                          font-weight-light
+                          text-grey-darken-2
+                          mb-2
+                        "
+                      >
+                        {{ item.content }}
+                      </div>
+                    </v-card-text>
+                  </v-card>
+                </v-hover>
+              </v-col>
+            </v-row>
           </v-col>
+
+          <v-col cols="12" lg="2"> </v-col>
         </v-row>
       </v-container>
     </v-main>
