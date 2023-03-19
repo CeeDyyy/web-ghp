414fb8755f85699912b41df58eaa8697d8d2c7a5
CeeDyyy <thanawat.ud@mail.kmutt.ac.th>
2023-03-19 22:24:59 +0700

    meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 
 export default defineNuxtConfig({
+    app: {
+        head: {
+            title: 'UPK Asia - Engineering',
+            meta: [
+                { name: 'description', content: 'UPK Asia - Engineering' },
+                { hid: 'og:image', property: 'og:image', content: '@/assets/logo.png' }
+            ],
+            charset: 'utf-8',
+            viewport: 'width=device-width, initial-scale=1',
+        }
+    },
     css: ['vuetify/styles', '@mdi/font/css/materialdesignicons.min.css', '@fortawesome/fontawesome-free/css/all.css'],
     build: {
         transpile: ['vuetify']
